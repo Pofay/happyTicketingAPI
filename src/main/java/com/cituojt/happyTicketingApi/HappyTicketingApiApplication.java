824799ced5cdf24cb88ede591a880782be4db174
pofay <gianCarlo.gilos@outlook.com>
2019-07-19 16:30:04 +0800

    Added Project files

---
+++ src/main/java/com/cituojt/happyTicketingApi/HappyTicketingApiApplication.java
@@ -0,0 +1,13 @@
+package com.cituojt.happyTicketingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTicketingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTicketingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cituojt/happyTicketingApi/HappyTicketingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cituojt.happyTicketingApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HappyTicketingApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
