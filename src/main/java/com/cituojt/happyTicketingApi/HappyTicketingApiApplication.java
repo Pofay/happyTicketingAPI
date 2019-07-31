64b3978b3744408a0c8ff4909b526a59c49dac2b
pofay <gianCarlo.gilos@outlook.com>
2019-07-31 15:47:08 +0800

    Setup Auth0 Authentication

--- src/main/java/com/cituojt/happyTicketingApi/HappyTicketingApiApplication.java
+++ src/main/java/com/cituojt/happyTicketingApi/HappyTicketingApiApplication.java
@@ -2,12 +2,15 @@ package com.cituojt.happyTicketingApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource("classpath:application.properties"), @PropertySource("classpath:auth0.properties") })
 public class HappyTicketingApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HappyTicketingApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HappyTicketingApiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/cituojt/happyTicketingApi/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.cituojt.happyTicketingApi.security;
+
+import java.util.Arrays;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        JwtWebSecurityConfigurer.forRS256(apiAudience, issuer).configure(http).authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/projects").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/projects").authenticated();
+    }
+}
