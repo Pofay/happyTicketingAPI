8329f9a4b14e4478cc55182d0c237650e12fe649
pofay <gianCarlo.gilos@gmail.com>
2019-09-11 13:00:50 +0800

    Changed allowed origins for everyone

--- src/main/java/com/cituojt/happyTicketingApi/security/SecurityConfig.java
+++ src/main/java/com/cituojt/happyTicketingApi/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("Authorization");
