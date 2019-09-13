7b0691f38d66640f2ab44827a2515c87d6760cc8
pofay <gianCarlo.gilos@gmail.com>
2019-09-13 16:40:36 +0800

    Removed unused packages

--- src/main/java/com/cituojt/happyTicketingApi/thirdParty/PusherRealtimeEmitter.java
+++ src/main/java/com/cituojt/happyTicketingApi/thirdParty/PusherRealtimeEmitter.java
@@ -2,7 +2,6 @@ package com.cituojt.happyTicketingApi.thirdParty;
 
 import com.cituojt.happyTicketingApi.controllers.RealtimeEmitter;
 import com.pusher.rest.Pusher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
