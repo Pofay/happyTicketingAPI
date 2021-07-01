6d6f2aacf27fd5873e62baa1c54aef3fab0f6f9e
Pofay <gianCarlo.gilos@gmail.com>
2021-07-01 20:33:27 +0800

    Updated to schema to fit Hibernate's way of doing things

--- db.sql
+++ db.sql
@@ -1,26 +1,28 @@
-CREATE TABLE USERS (
-    id SERIAL PRIMARY KEY,
+
+CREATE TABLE project_user(
+    user_id SERIAL PRIMARY KEY,
     email VARCHAR(255),
-    oauthId VARCHAR(255) 
+    oauth_id VARCHAR(255)
 );
 
-CREATE TABLE TASK(
-    id VARCHAR(255),
-    projectId BIGINT,
-    taskName VARCHAR(255),
-    taskStatus VARCHAR(255),
-    estimatedTime INT 
+CREATE TABLE task(
+    task_id VARCHAR(255),
+    project_id BIGINT,
+    task_name VARCHAR(255),
+    task_status VARCHAR(255),
+    assigned_to VARCHAR(255),
+    estimated_time INT
 );
 
-CREATE TABLE PROJECT_MEMBER(
-    userId BIGINT ,
-    projectId BIGINT,
-    memberRole VARCHAR(255),
-    memberStatus VARCHAR(255) 
+CREATE TABLE project_member(
+    user_user_id BIGINT ,
+    project_project_id BIGINT,
+    member_role VARCHAR(255),
+    member_status VARCHAR(255)
 );
 
-CREATE TABLE PROJECT(
-    projectId SERIAL,
-    projectName VARCHAR(255),
-    channelId VARCHAR(255) 
+CREATE TABLE project(
+    project_id SERIAL,
+    project_name VARCHAR(255),
+    channel_id VARCHAR(255)
 );
